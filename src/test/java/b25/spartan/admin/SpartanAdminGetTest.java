c31e45e957ef4cf5c11ec30254aa91845325ce9a
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 16:23:54 -0400

    Day 12: Serenity Admin

--- src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
@@ -1,4 +1,37 @@
 package b25.spartan.admin;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+@SerenityTest
 public class SpartanAdminGetTest {
+
+        //beforeAll is the same thing with beforeClass in testng
+        @BeforeAll
+        public static void init() {
+            RestAssured.baseURI = "http://44.201.121.105:7000";
+        }
+
+        @Test
+        public void getAllSpartan() {
+            given()
+                    .accept(ContentType.JSON)
+                    .and()
+                    .auth().basic("admin", "admin")
+                    .when()
+                    .get("/api/spartans")
+                    .then()
+                    .statusCode(200)
+                    .and()
+                    .contentType(ContentType.JSON);
+        }
+
+
 }
