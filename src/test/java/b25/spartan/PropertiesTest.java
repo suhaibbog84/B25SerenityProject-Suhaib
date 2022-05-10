53c9b3fc5d4516ba6394c67c14634299d3be66b0
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-10 12:39:07 -0400

    Day 13: Serenity properties Test

---
+++ src/test/java/b25/spartan/PropertiesTest.java
@@ -0,0 +1,15 @@
+package b25.spartan;
+
+import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
+
+public class PropertiesTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println(ConfigReader.getProperty("spartan.editor.username"));
+        System.out.println(ConfigReader.getProperty("serenity.project.name"));
+
+    }
+}
