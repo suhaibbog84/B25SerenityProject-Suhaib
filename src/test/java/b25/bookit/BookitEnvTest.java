32ab2fa2ab9feabe20d974716d410246f87fac89
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-10 13:06:07 -0400

    Day 13: Bookit Env Test

--- src/test/java/b25/bookit/BookitEnvTest.java
+++ src/test/java/b25/bookit/BookitEnvTest.java
@@ -1,4 +1,19 @@
 package b25.bookit;
 
+import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
+
 public class BookitEnvTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println(ConfigReader.getProperty("base_url"));
+        System.out.println(ConfigReader.getProperty("teacher_email"));
+        System.out.println(ConfigReader.getProperty("teacher_password"));
+
+    }
+
+
+
 }
--- src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
@@ -5,6 +5,7 @@ import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
 import net.serenitybdd.rest.Ensure;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static net.serenitybdd.rest.SerenityRest.given;
@@ -12,7 +13,7 @@ import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-
+@Disabled
 @SerenityTest
 public class SpartanAdminGetTest {
 
